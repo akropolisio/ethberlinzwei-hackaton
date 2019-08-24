19eb6b5388f22bf3ead8400d9b31cb4e677309dd
alekspickle <aleks.work2222@gmail.com>
2019-08-24 16:54:20 +0200

    lint: fix no keys

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -6,27 +6,27 @@ import { colors } from '../../utils';
 
 class About extends React.Component {
   renderTriangles = () => {
-    return Array.from(Array(10)).map(el => {
+    return Array.from(Array(10)).map((el, i) => {
       let top = Math.ceil(Math.random() * 90);
       let left = Math.ceil(Math.random() * 90);
       let w = Math.ceil(Math.random() * 200);
       let h = w;
       let color = Math.ceil(Math.random() * colors.length - 1);
-      return <Triangle top={top} left={left} w={w} h={h} color={colors[color]} />;
+      return <Triangle key={i} top={top} left={left} w={w} h={h} color={colors[color]} />;
     });
   };
   renderCircles = () => {
-    return Array.from(Array(10)).map(el => {
+    return Array.from(Array(10)).map((el, i) => {
       let top = Math.ceil(Math.random() * 90);
       let left = Math.ceil(Math.random() * 90);
       let w = Math.ceil(Math.random() * 100);
       let h = w;
       let color = Math.ceil(Math.random() * colors.length - 1);
-      return <Circle top={top} left={left} w={w} h={h} color={colors[color]} />;
+      return <Circle key={i} top={top} left={left} w={w} h={h} color={colors[color]} />;
     });
   };
   renderAngles = () => {
-    return Array.from(Array(20)).map(el => {
+    return Array.from(Array(20)).map((el, i) => {
       let top = Math.ceil(Math.random() * 30);
       let left = Math.ceil(Math.random() * 90);
       let w = 1 + Math.ceil(Math.random() * 10);
@@ -34,7 +34,7 @@ class About extends React.Component {
       let color = Math.ceil(Math.random() * colors.length - 1);
       let rotate = -5 - Math.ceil(Math.random() * 300);
 
-      return <Angle top={top} left={left} w={w} h={h} color={colors[color]} rotate={rotate} />;
+      return <Angle key={i} top={top} left={left} w={w} h={h} color={colors[color]} rotate={rotate} />;
     });
   };
 
