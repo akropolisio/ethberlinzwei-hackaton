402e42282cae7462bf9b855f3cee4144f58b6365
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-08-24 14:36:59 +0200

    StandartTokenMocj

--- test/Mocks/StandardTokenMock.sol
+++ test/Mocks/StandardTokenMock.sol
@@ -1,7 +1,7 @@
-pragma solidity ^0.4.24;
-import "../Tokens/StandardToken.sol";
+pragma solidity ^0.5.0;
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/StandaloneERC20.sol";
 
-contract StandardTokenMock is StandardToken {
+contract StandardTokenMock is StandaloneERC20 {
   function setBalance(address _address, uint amount) public {
     balances[_address] = amount;
   }
