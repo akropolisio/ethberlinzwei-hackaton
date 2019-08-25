7d8cf6ae87dd6da4d5370c1dec587ff765d9b4c1
Alex Cher <ilgiz.gimaltdinov@gmail.com>
2019-08-25 13:42:02 +0700

    Updates

---
+++ precache-manifest.5450ad1727e4e11f55be0e15ee84f580.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "610fafed1a4b60db1f6331b4680fce80",
+    "url": "/static/media/trump1.610fafed.png"
+  },
+  {
+    "revision": "e06c880b03e7bf722d42",
+    "url": "/static/css/main.e6181c91.chunk.css"
+  },
+  {
+    "revision": "53afe1c9654de64e3117541e232ea73b",
+    "url": "/static/media/arrow-down.53afe1c9.png"
+  },
+  {
+    "revision": "441d55df9f32d1eb2b5a",
+    "url": "/static/js/1.441d55df.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f236d5f2d7168e5bdf9191ec4a4758a",
+    "url": "/static/media/trump3.1f236d5f.png"
+  },
+  {
+    "revision": "7b20ea55160f2757700a7a01d0bf74a7",
+    "url": "/static/media/buffett1.7b20ea55.png"
+  },
+  {
+    "revision": "e06c880b03e7bf722d42",
+    "url": "/static/js/main.e06c880b.chunk.js"
+  },
+  {
+    "revision": "607cfa6d0e41f6d19f77f391d9f6c8b2",
+    "url": "/static/media/buffett5.607cfa6d.png"
+  },
+  {
+    "revision": "5a73b9d8fa1c3b9adebb1b46ff508293",
+    "url": "/static/media/nouriel1.5a73b9d8.png"
+  },
+  {
+    "revision": "91cbe8709a24cf4c4b27b8e6d8c0c669",
+    "url": "/static/media/nouriel2.91cbe870.png"
+  },
+  {
+    "revision": "ab7d250b9e3eb667ae26533ecc81518e",
+    "url": "/static/media/loader.ab7d250b.svg"
+  },
+  {
+    "revision": "441d55df9f32d1eb2b5a",
+    "url": "/static/css/1.96f4f07f.chunk.css"
+  },
+  {
+    "revision": "8673347d8ac97e4a50dc65b2cd8c3465",
+    "url": "/index.html"
+  }
+];
